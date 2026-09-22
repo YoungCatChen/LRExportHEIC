26abea3e0682d7594796f06a68a44797cf2446dd
YoungCatChen <YoungCatChen@users.noreply.github.com>
2026-09-21 17:32:07 -0700

    Avoid duplicate CI runs for release tags
    Limit CI pushes to branches because release tags point at commits already validated on their source branches. Also credit @uannzi in the plugin information dialog.

--- LRPlugin/PluginInfoProvider.lua
+++ LRPlugin/PluginInfoProvider.lua
@@ -9,7 +9,7 @@ return {
           },
           viewFactory:spacer { height = 12 },
           viewFactory:static_text {
-            title = 'Created by Manu Wallner (GitHub: @milch) and YoungCat (GitHub: @YoungCatChen).',
+            title = 'Created by Manu Wallner (GitHub: @milch) and YoungCat (GitHub: @YoungCatChen). Contributions by @uannzi.',
           },
         },
       },
